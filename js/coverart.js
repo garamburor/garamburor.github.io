37c8752cef27f11dfcb96503b6b909600eaba16f
garamburor <garamburor@hotmail.com>
2024-09-15 22:55:02 +0200

    Intro animation progress.

--- js/coverart.js
+++ js/coverart.js
@@ -1,29 +1,47 @@
+// frame rate
+let fps = 35;
+
+/* Sphere animation */
+// sine amplitude (radius of sphere)
 let r = 0; 
+// Sin array
 let waves = []; 
-let num = 6; 
+// Number of sines
+let num = 6;
+// Phase shift between sines 
 let step = 30;
-let msTime = 0;
+
+/* Intro animation */
+let counter = 0;
 let j = 0;
 
+// p5js setup
 function setup() {
+  // Set canvas size
   canvas = createCanvas(windowWidth, windowHeight);
-  frameRate(35);
+  // Set frame rate
+  frameRate(fps);
+  // Refresh variables that depend on window size
   windowResized();
+  // Set color of strokes
   stroke(0, 0, 255);
+  // Set angle with degrees instead of radians
   angleMode(DEGREES);
+  // Create wave objects
   for (let i=0; i<num; i++) {
     waves[i] = new Wave(i*step);
   }
 }
 
+/* Main loop */
 function draw() {
-  // msTime = Math.floor(millis() * 1e-3);
   translate(width/2, height/2);
   canvas.clear();
   // Sphere Animation
   sphereAnim();
 }
 
+/* Sphere animation loop */
 function sphereAnim() {
   noFill();
   strokeWeight(1);
@@ -32,7 +50,8 @@ function sphereAnim() {
     waves[i].move();
   }
 }
-// Always resize the canvas to fill the browser window.
+
+/* Adjust to window size  dynamically */
 function windowResized() {
     // make sure position is correct
     //canvas.position(-34,3.25);
@@ -43,44 +62,65 @@ function windowResized() {
     canvas.position(rect.left + r * 0.14, rect.top + r * 0.88);
 }
 
-function introAnim() {
-    switch(j) {
-      case 0: // Motif intro
-        var el = document.getElementById('span1');
-        const rect = el.getBoundingClientRect();
-        canvas.position(width * 0.5, rect.top + r * 0.88);
-        document.getElementById("intro1").textContent = '';
-        document.getElementById("intro2").textContent = "";
-        document.getElementById("subt").textContent = '';
-        windowResized();
-        break;
-      case 2: // greeting
-        document.getElementById("intro1").textContent = 'H';
-        document.getElementById("intro2").textContent = "LA";
-        document.getElementById("subt").textContent = '';
-        windowResized();
-        break;
-      case 3: // I'm
-        document.getElementById("intro1").textContent = 'S';
-        document.getElementById("intro2").textContent = "Y";
-        document.getElementById("subt").textContent = '';
-        windowResized();
-        break;
-      case 4: // Cover
-        document.getElementById("intro1").textContent = 'GUILLERM';
-        document.getElementById("intro2").textContent = "";
-        document.getElementById("subt").textContent = 'ARAMBURO RODRIGUEZ';
-        windowResized();
-        clearInterval(id);
-        break;
-      default:
-        break;
+function textChange() {
+    if (counter >= fps) {
+      switch(j) {
+        case 0: // Motif intro
+          var el = document.getElementById('span1');
+          var rect = el.getBoundingClientRect();
+          canvas.position(width * 0.5, rect.top + r * 0.88);
+          document.getElementById("intro1").textContent = '';
+          document.getElementById("intro2").textContent = "";
+          document.getElementById("subt").textContent = '';
+          windowResized();
+          break;
+        case 1: // greeting
+          document.getElementById("intro1").textContent = 'H';
+          document.getElementById("intro2").textContent = "LA";
+          document.getElementById("subt").textContent = '';
+          windowResized();
+          break;
+        case 2: // I'm
+          document.getElementById("intro1").textContent = 'S';
+          document.getElementById("intro2").textContent = "Y";
+          document.getElementById("subt").textContent = '';
+          windowResized();
+          break;
+        case 3: // Cover
+          document.getElementById('span1').style.position = "sticky";
+          document.getElementById("intro1").textContent = 'GUILLERM';
+          document.getElementById("intro2").textContent = "";
+          document.getElementById("subt").style.opacity = 0;
+          document.getElementById("subt").textContent = 'ARAMBURO RODRIGUEZ';
+          windowResized();
+          break;
+        default:
+          break;
+      }
+      j += 1;
+      counter = 0;
+    }
+
+    if (j < 4) {
+      textFadeInNOut();
+    }
+    else if (j < 5) {
+      document.getElementById("intro1").style.opacity = 1. - 1./Math.exp(7 * counter/fps);
+      document.getElementById("intro2").style.opacity = 1. - 1./Math.exp(7 * counter/fps);
+      document.getElementById("subt").style.opacity = 1. - 1./Math.exp(4 * counter/fps);
+    }
+    else {
+      clearInterval(id1); // fix
     }
     
-    // location.reload();
-    j += 1; 
+    counter += 1; 
 }
 
-let id = null;
-//clearInterval(id);
-id = setInterval(introAnim, 1000);
+function textFadeInNOut() {
+  document.getElementById("intro1").style.opacity = Math.tanh(2 * Math.sin(Math.PI * counter/fps));
+  document.getElementById("intro2").style.opacity = Math.tanh(2 * Math.sin(Math.PI * counter/fps));
+}
+
+let id1 = null;
+clearInterval(id1);
+id1 = setInterval(textChange, 1000/fps);
